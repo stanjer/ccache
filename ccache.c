22ba0903b3870f62b116798917f7d8403f97dfed
Anders Björklund <anders@itension.se>
2015-11-29 22:37:12 +0100

    Don't try to use memcached for split dwarf

--- ccache.c
+++ ccache.c
@@ -1194,6 +1194,7 @@ to_cache(struct args *args)
 
 #ifdef HAVE_LIBMEMCACHED
 	if (strlen(conf->memcached_conf) > 0 && !conf->read_only_memcached &&
+	    !using_split_dwarf && /* no support for the dwo files just yet */
 	    !generating_coverage) { /* coverage refers to local paths anyway */
 		cc_log("Storing %s in memcached", cached_key);
 		if (!read_file(cached_obj, 0, &data_obj, &size_obj)) {
@@ -1799,7 +1800,9 @@ from_cache(enum fromcache_call_mode mode, bool put_object_in_manifest)
 	if (stat(cached_obj, &st) != 0) {
 		cc_log("Object file %s not in cache", cached_obj);
 #if HAVE_LIBMEMCACHED
-		if (strlen(conf->memcached_conf) > 0 && !generating_coverage) {
+		if (strlen(conf->memcached_conf) > 0 &&
+		    !using_split_dwarf &&
+		    !generating_coverage) {
 			cc_log("Getting %s from memcached", cached_key);
 			cache = memccached_get(cached_key,
 			                       &data_obj, &data_stderr, &data_dia, &data_dep,
